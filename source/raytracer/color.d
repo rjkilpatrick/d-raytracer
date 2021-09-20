cc3a19281916394f5633d453e302777fd77d84b4
John Kilpatrick <john.kilpatrick@outlook.com>
2021-09-20 12:38:11 +0100

    feat: Add color class

---
+++ source/raytracer/color.d
@@ -0,0 +1,40 @@
+module raytracer.color;
+
+import raytracer.vec3;
+
+
+/// 3-vector of RGB color
+class Color : Vec3 {
+
+    /// Returns black Color
+    this() pure {
+        super();
+    }
+
+    /// Returns Color filled with r, g, b
+    this(double r, double g, double b) pure {
+        super(r, g, b);
+    }
+
+    import std.format : FormatSpec, FormatException;
+
+    override void toString(scope void delegate(const(char)[]) sink, FormatSpec!char fmt) const {
+
+        switch (fmt.spec) {
+        case 's':
+            import std.conv : to;
+
+            sink((255.999 * this.x).to!int
+                    .to!string);
+            sink(" ");
+            sink((255.999 * this.y).to!int
+                    .to!string);
+            sink(" ");
+            sink((255.999 * this.z).to!int
+                    .to!string);
+            break;
+        default:
+            throw new FormatException("Unknown format specifier: %" ~ fmt.spec);
+        }
+    }
+}
